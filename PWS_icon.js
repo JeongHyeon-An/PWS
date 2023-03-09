b4560537c259500641f952ed6c2c20f9de00610d
JeongHyeon-An <86676454+JeongHyeon-An@users.noreply.github.com>
2023-03-09 09:19:46 +0900

    Create PWS_icon.js

---
+++ PWS_icon.js
@@ -0,0 +1,13 @@
+var IconMapping=[ "wi-darksky-tornado", "wi-hurricane",
+"wi-hurricane","wi-owm-11d","wi-owm-11d",
+"wi-wu-rain","wi-wu-sleet","wi-wu-sleet","wi-wu-sleet",
+"wi-wu-rain","wi-wu-chancesleet","wi-wu-rain","wi-wu-rain",
+"wi-wu-flurries","wi-wu-flurries","wi-wu-snow","wi-wu-snow",
+"wi-wu-sleet","wi-wu-sleet","wi-sandstorm","wi-wu-hazy",
+"wi-wu-hazy","wi-wu-hazy","wi-darksky-wind","wi-darksky-wind",
+"wi-wu-sleet","wi-wu-cloudy","wi-wu-mostlycloudy",
+"wi-wu-mostlycloudy","wi-wu-partlycloudy","wi-wu-partlycloudy",
+"wi-wu-clear","wi-wu-sunny","wi-owm-02d","wi-owm-02d","wi-wu-rain",
+"wi-wu-sunny","wi-darksky-thunderstorm","wi-darksky-thunderstorm","wi-wu-chancerain",
+"wi-owm-10d","wi-wu-flurries","wi-wu-snow","wi-wu-snow","wi-na",
+"wi-wu-chancerain","wi-wu-snow","wi-wu-tstorms"];
